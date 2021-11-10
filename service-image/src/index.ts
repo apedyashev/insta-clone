d5c7ea63a7f5b37d30d4ec5296076339f11b9768
Alexey Pedyashev <alexey.pedyashev@gmail.com>
2021-11-10 22:09:00 +0100

    feat: rabbit mq test

--- service-image/src/index.ts
+++ service-image/src/index.ts
@@ -24,3 +24,28 @@ logger.error('log4js log error');
 
 
 app.listen(port, () => console.log(`Running on port ${port}`));
+
+const amqp = require('amqplib');
+
+amqp.connect('amqp://localhost').then((connection) => {
+    connection.createChannel().then((channel) => {
+        const queue = 'hello';
+        const msg = 'Hello World!';
+
+        channel.assertQueue(queue, {
+            durable: false
+        });
+        channel.sendToQueue(queue, Buffer.from(msg));
+
+        console.log(" [x] Sent %s", msg);
+    }).catch((error) => {
+        throw error;
+    });
+
+    setTimeout(function() {
+        connection.close();
+        process.exit(0);
+    }, 500);
+}).catch((error) => {
+    throw error;
+});
